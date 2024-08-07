c6ec1efe158094de002188d3fd0c924215b13ff9
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-08-07 21:33:07 +0700

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,30 @@
+public class Test2 {
+    public static void main(String[] args) {
+        IntegerNode head =null;
+        System.out.println("head = "+head);
+        IntegerNode lst1 = new IntegerNode(10);
+        head = lst1;
+        System.out.println("head = "+head);  
+        
+        
+
+        IntegerNode lst2 = new IntegerNode(20);
+        lst1.setNext(lst2); 
+        lst2.setItem(lst2.getItem());
+        
+        IntegerNode lst3 = new IntegerNode(30);
+        lst3.setNext(head);
+        head = lst3;
+        System.out.println("head = "+head);
+        System.out.println("lst3 = "+lst3);
+        System.out.println("lst3 value = "+lst3.getItem());
+        System.out.println("lst3 Next = "+lst3.getNext()+"\n");
+        
+        System.out.println("lst1 = "+lst1);
+        System.out.println("lst1 value = "+lst1.getItem());
+        System.out.println("lst1 Next = "+lst1.getNext()+"\n");
+        System.out.println("lst2 = "+lst2);
+        System.out.println("lst2 value = "+lst2.getItem());
+    }
+        
+}
